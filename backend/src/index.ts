cfc933f401c9d0115b7578a9ba558cbb54959670
Md Saquib Hayat <saquib.md@techurate.com>
2024-03-11 12:30:58 +0530

    project created

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/api/test", (req: Request, res: Response) => {
+  res.json({ message: "hello from express endpoints!" });
+});
+
+app.listen(8000, () => {
+  console.log("server is running on 8000");
+});
